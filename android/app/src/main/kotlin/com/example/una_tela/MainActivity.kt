16dd4ed5d75aac65cea8030cfd3bce69b7adc9a7
Éder Germanio <desenvolvimento@gsate.com.br>
2020-09-17 22:08:48 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/una_tela/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.una_tela
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/02-helloworld.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Text('Éder Germanio Alves Campos RA: 41923020!',
+        textDirection: TextDirection.ltr),
+  );
+}
